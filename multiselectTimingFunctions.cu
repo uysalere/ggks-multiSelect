b6ab057b4bc88cea9b5fb64d385cbdee071e9949
Erik Opavsky <opavskye@cs.grinnell.edu>
2012-10-28 13:47:09 -0500

    updated

--- multiselectTimingFunctions.cu
+++ multiselectTimingFunctions.cu
@@ -3,6 +3,10 @@
 
 #define MAX_THREADS_PER_BLOCK 1024
 
+#define CUDA_CALL(x) do { if((x) != cudaSuccess) {    \
+      printf("Error at %s:%d\n",__FILE__,__LINE__);     \
+      return EXIT_FAILURE;}} while(0)
+
 template <typename T>
  struct results_t {
   float time;
@@ -34,13 +38,14 @@ void wrapupForTiming(cudaEvent_t &start, cudaEvent_t &stop, float time, results_
 /////////////////////////////////////////////////////////////////
 
 
-  template <typename T>
-  __global__ void copyInChunk (T * outputVector, T * inputVector, uint * kList, int kListCount) {
-   
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < kListCount)
-      *(outputVector + idx) = *(inputVector + kListCount - *(kList + idx));
-  }
+template <typename T>
+__global__ void copyInChunk(T * outputVector, T * inputVector, uint * kList, uint kListCount, uint numElements) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < kListCount) 
+    *(outputVector + idx) = *(inputVector + (numElements - (*(kList + idx))));
+  
+}
 
 template<typename T>
 results_t<T>* timeSortAndChooseMultiselect(T *h_vec, uint numElements, uint * kVals, uint kCount) {
@@ -52,38 +57,38 @@ results_t<T>* timeSortAndChooseMultiselect(T *h_vec, uint numElements, uint * kV
   setupForTiming(start, stop, h_vec, &d_vec, &result, numElements, kCount);
 
   cudaEventRecord(start, 0);
-
   thrust::device_ptr<T> dev_ptr(d_vec);
   thrust::sort(dev_ptr, dev_ptr + numElements);
 
+  /*
   for (int i = 0; i < kCount; i++)
     cudaMemcpy(result->vals + i, d_vec + (numElements - kVals[i]), sizeof (T), cudaMemcpyDeviceToHost);
+  */
 
-  /*
   T * d_output;
-  T * d_kList;
-  CUDA_CALL(cudaMalloc (&d_output, kCount * sizeof (T)));
-  CUDA_CALL(cudaMalloc(&d_kList, kCount * sizeof(uint)));
-  CUDA_CALL(cudaMemcpy (d_kList, kVals, kCount * sizeof (uint), cudaMemcpyHostToDevice));
+  uint * d_kList;
 
-  threads = MAX_THREADS_PER_BLOCK;
+  cudaMalloc (&d_output, kCount * sizeof (T));
+  cudaMalloc (&d_kList, kCount * sizeof(uint));
+  cudaMemcpy (d_kList, kVals, kCount * sizeof (uint), cudaMemcpyHostToDevice);
+
+  int threads = MAX_THREADS_PER_BLOCK;
   if (kCount < threads)
     threads = kCount;
-  blocks = (int) ceil (kCount / (float) threads);
+  int blocks = (int) ceil (kCount / (float) threads);
 
-  copyInChunk<<<blocks, threads>>>(d_output, dev_ptr, d_kList, kCount);
+  copyInChunk<T><<<blocks, threads>>>(d_output, d_vec, d_kList, kCount, numElements);
   cudaMemcpy (result->vals, d_output, kCount * sizeof (T), cudaMemcpyDeviceToHost);
 
   cudaFree(d_output);
   cudaFree(d_kList); 
-  */
-  cudaFree(d_vec);
-
+   
   cudaEventRecord(stop, 0);
   cudaEventSynchronize(stop);
   cudaEventElapsedTime(&time, start, stop);
 
   wrapupForTiming(start, stop, time, result);
+  cudaFree(d_vec);
   return result;
 }
 
