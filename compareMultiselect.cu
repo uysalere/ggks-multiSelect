34842ddc73bad042fda227f7b531c72620eff4a6
Erik Opavsky <opavskye@cs.grinnell.edu>
2012-11-15 09:57:59 -0600

    working

--- compareMultiselect.cu
+++ compareMultiselect.cu
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <fstream>
 #include <sys/time.h>
+#include <iomanip>
+#include <unistd.h>
 
 #include <algorithm>
 //Include various thrust items that are used
@@ -34,12 +36,16 @@
 #include "multiselectTimingFunctions.cu"
 
 #define NUMBEROFALGORITHMS 3
-char* namesOfMultiselectTimingFunctions[NUMBEROFALGORITHMS] = {"Sort and Choose Multiselect", "Bucket Multiselect", "Naive Bucket Multiselect"};
+char* namesOfMultiselectTimingFunctions[NUMBEROFALGORITHMS] = {"Sort and Choose Multiselect", "Bucket Multiselect", "Stable Sort and Choose Multiselect"};
 
+union uf{
+  float f;
+  unsigned int u;
+};
 
 using namespace std;
 template<typename T>
-void compareMultiselectAlgorithms(uint size, uint * kVals, uint kListCount, uint numTests, uint *algorithmsToTest, uint generateType, uint kGenerateType, char* fileNamecsv) {
+void compareMultiselectAlgorithms(uint size, uint * kVals, uint kListCount, uint numTests, uint *algorithmsToTest, uint generateType, uint kGenerateType, char* fileNamecsv, time_t srandSeed, unsigned long long originalSeed, uint mainSeed, time_t generatorSeed, unsigned long long seed) {
   T *h_vec, *h_vec_copy;
   float timeArray[NUMBEROFALGORITHMS][numTests];
   T * resultsArray[NUMBEROFALGORITHMS][numTests];
@@ -49,12 +55,11 @@ void compareMultiselectAlgorithms(uint size, uint * kVals, uint kListCount, uint
   uint i,j,m,x;
   int runOrder[NUMBEROFALGORITHMS];
 
-  unsigned long long seed;
   results_t<T> *temp;
   ofstream fileCsv;
-  timeval t1;
+  //timeval t1;
  
-  typedef results_t<T>* (*ptrToTimingFunction)(T*, uint, uint *, uint);
+  typedef results_t<T>* (*ptrToTimingFunction)(T*, uint, uint *, uint, uint *);
   typedef void (*ptrToGeneratingFunction)(T*, uint, curandGenerator_t);
 
   //these are the functions that can be called
@@ -81,30 +86,50 @@ void compareMultiselectAlgorithms(uint size, uint * kVals, uint kListCount, uint
 
   //create the random generator.
   curandGenerator_t generator;
-  srand(unsigned(time(NULL)));
+  // time_t generatorSeed = time(NULL);
+  srand (unsigned (generatorSeed));
 
   printf("The distribution is: %s\n", namesOfGeneratingFunctions[generateType]);
   printf("The k distribution is: %s\n", namesOfKGenerators[kGenerateType]);
-
-  //*/******************* START RUNNING TESTS *************
-  /***********************************************/
-
   for(i = 0; i < numTests; i++) {
     // cudaDeviceReset();
-    gettimeofday(&t1, NULL);
-    seed = t1.tv_usec * t1.tv_sec;
+    // gettimeofday(&t1, NULL);
+    // seed = t1.tv_usec * t1.tv_sec;
     
     for(m = 0; m < NUMBEROFALGORITHMS;m++)
       runOrder[m] = m;
     
     std::random_shuffle(runOrder, runOrder + NUMBEROFALGORITHMS);
-    fileCsv << size << "," << kVals[0] << "," << kVals[kListCount - 1] << "," << kListCount << "," << (100*((float)kListCount/size)) << "," << namesOfGeneratingFunctions[generateType] << "," << namesOfKGenerators[kGenerateType] << "," << seed << ",";
+    //  fileCsv << size << "," << kVals[0] << "," << kVals[kListCount - 1] << "," << kListCount << "," << (100*((float)kListCount/size)) << "," << namesOfGeneratingFunctions[generateType] << "," << namesOfKGenerators[kGenerateType] << ", originalSeed " << originalSeed << ", generatorSeed " << generatorSeed << ", seed " << seed << ",";
     curandCreateGenerator(&generator, CURAND_RNG_PSEUDO_DEFAULT);
     curandSetPseudoRandomGeneratorSeed(generator,seed);
     printf("Running test %u of %u for size: %u and numK: %u\n", i + 1, numTests, size, kListCount);
     //generate the random vector using the specified distribution
     arrayOfGenerators[generateType](h_vec, size, generator);
 
+    // print the vector
+    /*
+    ofstream vectorFileCsv;
+    uf bits;
+    vectorFileCsv.open("vector3.csv", ios_base::app);
+    for (int z = 0; z < size; z++) {
+      float f = *(h_vec + z);
+      vectorFileCsv << "\'";
+      bits.f= f;
+      for(int i = 0; i < 32; i++){
+      /*
+      if(! (i % 4)){
+      vectorFileCsv << "|";
+      }
+    */
+    /*
+      vectorFileCsv << ((bits.u >> (32 - 1 - i)) & 0x1);
+      }
+      vectorFileCsv << "\'\n";
+      }
+      
+      vectorFileCsv.close();
+    */
     //copy the vector to h_vec_copy, which will be used to restore it later
     memcpy(h_vec_copy, h_vec, size * sizeof(T));
 
@@ -117,8 +142,11 @@ void compareMultiselectAlgorithms(uint size, uint * kVals, uint kListCount, uint
 
         //run timing function j
         printf("TESTING: %u\n", j);
-        temp = arrayOfTimingFunctions[j](h_vec_copy, size, kVals, kListCount);
-
+        //  printf ("kVals[72] = %u\n", kVals[72]);
+        //  kVals[71] = 65933979;
+        //  kVals[73] = 65933981;
+        temp = arrayOfTimingFunctions[j](h_vec_copy, size, kVals, kListCount, &mainSeed);
+        printf ("%u, %u\n", kVals[71], kVals[73]);
         //record the time result
         timeArray[j][i] = temp->time;
         //record the value returned
@@ -140,23 +168,14 @@ void compareMultiselectAlgorithms(uint size, uint * kVals, uint kListCount, uint
       if(algorithmsToTest[x])
         fileCsv << namesOfMultiselectTimingFunctions[x] << "," << timeArray[x][i] << ",";
 
+    // check if results are correct
     uint flag = 0;
     for(m = 1; m < NUMBEROFALGORITHMS;m++)
       if(algorithmsToTest[m])
         for (j = 0; j < kListCount; j++) {
-          if(resultsArray[m][i][j] != resultsArray[0][i][j]) {
+          T tempResult = resultsArray[0][i][j];
+          if(resultsArray[m][i][j] != tempResult)
             flag++;
-            fileCsv << "\nERROR ON TEST " << i << " of " << numTests << " tests!!!!!\n";
-            fileCsv << "vector size = " << size << "\nvector seed = " << seed << "\n";
-            fileCsv << "kListCount = " << kListCount << "\n";
-            fileCsv << "wrong k = " << kVals[j] << " kIndex = " << j << " wrong result = " << resultsArray[m][i][j] << " correct result = " <<  resultsArray[0][i][j] << "\n";
-            std::cout <<namesOfMultiselectTimingFunctions[m] <<" did not return the correct answer on test " << i + 1 << " at k[" << j << "].  It got "<< resultsArray[m][i][j];
-            std::cout << " instead of " << resultsArray[0][i][j] << ".\n" ;
-            std::cout << "RESULT:\t";
-            PrintFunctions::printBinary(resultsArray[m][i][j]);
-            std::cout << "Right:\t";
-            PrintFunctions::printBinary(resultsArray[0][i][j]);
-          }
         }
 
     fileCsv << flag << "\n";
@@ -184,12 +203,34 @@ void compareMultiselectAlgorithms(uint size, uint * kVals, uint kListCount, uint
     if(algorithmsToTest[i])
       printf("%s won %u times\n", namesOfMultiselectTimingFunctions[i], timesWon[i]);
 
+  for(i = 0; i < numTests; i++)
+    for(j = 1; j < NUMBEROFALGORITHMS; j++)
+      for (m = 0; m < kListCount; m++)
+        if(algorithmsToTest[j])
+          if(resultsArray[j][i][m] != resultsArray[0][i][m]) {
 
-  // free results
+            std::cout << "wrong k:" << kVals[354] << ".\n";
+
+
+            std::cout <<namesOfMultiselectTimingFunctions[j] <<" did not return the correct answer on test " << i + 1 << " at k[" << m << "].  It got "<< resultsArray[j][i][m];
+            std::cout << " instead of " << resultsArray[0][i][m] << ".\n" ;
+            std::cout << "RESULT:\t";
+            PrintFunctions::printBinary(resultsArray[j][i][m]);
+            std::cout << "Right:\t";
+            PrintFunctions::printBinary(resultsArray[0][i][m]);
+            std::cout << size << "," << kVals[0] << "," << kVals[kListCount - 1] << "," << kListCount << "," << (100*((float)kListCount/size)) << "," << namesOfGeneratingFunctions[generateType] << "," << namesOfKGenerators[kGenerateType] << ", originalSeed " << originalSeed << ", generatorSeed " << generatorSeed << ", seed " << seed << ", mainSeed " << mainSeed << "\n";
+
+            // print to file
+            fileCsv << "\nERROR!!! on" << namesOfMultiselectTimingFunctions[j] << " did not return the correct answer on test " << i + 1 << " at k[" << m << "]=" << kVals[m] << "\n" ; 
+            fileCsv << size << "," << kVals[0] << "," << kVals[kListCount - 1] << ", kListCount = " << kListCount << "," << (100*((float)kListCount/size)) << "," << namesOfGeneratingFunctions[generateType] << "," << namesOfKGenerators[kGenerateType] << ", originalSeed " << originalSeed << ", generatorSeed " << generatorSeed << ", seed " << seed << ", mainSeed " << mainSeed << "\n";
+            
+          }
+
   for(i = 0; i < numTests; i++) 
     for(m = 0; m < NUMBEROFALGORITHMS; m++) 
       if(algorithmsToTest[m])
         free(resultsArray[m][i]);
+
 
   //free h_vec and h_vec_copy
   free(h_vec);
@@ -200,35 +241,22 @@ void compareMultiselectAlgorithms(uint size, uint * kVals, uint kListCount, uint
 
 
 template<typename T>
-void runTests (uint generateType, char* fileName, uint startPower, uint stopPower, uint timesToTestEachK, 
-               uint kDistribution, uint startK, uint stopK, uint kJump) {
-  uint algorithmsToRun[NUMBEROFALGORITHMS]= {1, 1, 0};
+void runTests (uint generateType, char* fileName, uint startPower, uint stopPower, uint timesToTestEachK, uint kDistribution, uint startK, uint stopK, uint kJump, time_t srandSeed, unsigned long long originalSeed, uint mainSeed, time_t generatorSeed, unsigned long long seed) {
+  uint algorithmsToRun[NUMBEROFALGORITHMS]= {1, 1, 1};
   uint size;
   uint i;
   uint arrayOfKs[stopK+1];
   
   
-  for(size = (1 << startPower); size <= (1 << stopPower); size *= 2) {
-    /*
-    //calculate k values
-    arrayOfKs[0] = 2;
-    //  arrayOfKs[1] = (uint) (.01 * (float) size);
-    //  arrayOfKs[2] = (uint) (.025 * (float) size);
-    for(i = 1; i <= num - 2; i++) 
-    arrayOfKs[i] = (uint) (( i / (float) num ) * size);
-    
-    //  arrayOfKs[num-3] = (uint) (.9975 * (float) size);
-    //  arrayOfKs[num-2] = (uint) (.999 * (float) size);
-    arrayOfKs[num-1] = (uint) (size - 2); 
-    */
-    unsigned long long seed;
-    timeval t1;
-    gettimeofday(&t1, NULL);
-    seed = t1.tv_usec * t1.tv_sec;
+  for (size = (1 << startPower); size <= (1 << stopPower); size *= 2) {
+
+    // timeval t1;
+    // gettimeofday(&t1, NULL);
+    // originalSeed = t1.tv_usec * t1.tv_sec;
     curandGenerator_t generator;
-    srand(unsigned(time(NULL)));
+    srand(unsigned(srandSeed));
     curandCreateGenerator(&generator, CURAND_RNG_PSEUDO_DEFAULT);
-    curandSetPseudoRandomGeneratorSeed(generator,seed);
+    curandSetPseudoRandomGeneratorSeed(generator,originalSeed);
 
     arrayOfKDistributionGenerators[kDistribution](arrayOfKs, stopK, size, generator);
 
@@ -245,13 +273,25 @@ void runTests (uint generateType, char* fileName, uint startPower, uint stopPowe
       cudaDeviceReset();
       cudaThreadExit();
       printf("NOW ADDING ANOTHER K\n\n");
-      compareMultiselectAlgorithms<T>(size, arrayOfKs, i, timesToTestEachK, algorithmsToRun, generateType, kDistribution, fileName);
+      compareMultiselectAlgorithms<T>(size, arrayOfKs, i, timesToTestEachK, algorithmsToRun, generateType, kDistribution, fileName, srandSeed, originalSeed, mainSeed, generatorSeed, seed );
     }
   }
 }
 
 
 int main (int argc, char *argv[]) {
+  //kDistributionSeed 385095184934058, generatorSeed 1351810014, srandSeed 1351808817, arraySeed 621812329289790, randomSampleSeed 2390577621
+  // originalSeed = kDistributionSeed
+  unsigned long long kDistributionSeed = 87802604328220; // THIS MUST BE FIXED TO GET ERROR
+  time_t generatorSeed = time(NULL);
+  //time_t generatorSeed = time(NULL); //1351465169; // THIS CAN BE RANDOM
+  time_t srandSeed = time (NULL);
+  //time_t srandSeed = time (NULL);//1351465069;  // this can also be random
+  // seed = arraySeed
+  unsigned long long arraySeed = 1313572239387928; // THIS MUST BE FIXED TO GET ERROR
+  // randomSampleSeed = mainSeed
+  uint randomSampleSeed = 453328713; // THIS MUST BE FIXED TO GET ERROR
+
   char *fileName, *hostName, *typeString;
 
   uint testCount;
@@ -302,19 +342,21 @@ int main (int argc, char *argv[]) {
     break;
   }
 
+
   snprintf(fileName, 128, "%s %s k-dist:%s 2^%d to 2^%d (%d:%d:%d) %d-tests on %s at %s", typeString, getDistributionOptions(type, distributionType), getKDistributionOptions(kDistribution), startPower, stopPower, startK, jumpK, stopK, testCount, hostName, asctime(timeinfo));
   printf("File Name: %s \n", fileName);
   //printf("Please enter filename now: ");
 
+  //void runTests (uint generateType, char* fileName, uint startPower, uint stopPower, uint timesToTestEachK, uint kDistribution, uint startK, uint stopK, uint kJump, time_t srandSeed, unsigned long long originalSeed, uint mainSeed, time_t generatorSeed, unsigned long long seed) {
   switch(type){
   case 0:
-    runTests<float>(distributionType,fileName,startPower,stopPower,testCount,kDistribution,startK,stopK,jumpK);
+    runTests<float>(distributionType,fileName,startPower,stopPower,testCount,kDistribution,startK,stopK,jumpK, srandSeed, kDistributionSeed, randomSampleSeed, generatorSeed, arraySeed);
     break;
   case 1:
-    runTests<double>(distributionType,fileName,startPower,stopPower,testCount,kDistribution,startK,stopK,jumpK);
+    runTests<double>(distributionType,fileName,startPower,stopPower,testCount,kDistribution,startK,stopK,jumpK, srandSeed, kDistributionSeed, randomSampleSeed, generatorSeed, arraySeed);
     break;
   case 2:
-    runTests<uint>(distributionType,fileName,startPower,stopPower,testCount,kDistribution,startK,stopK,jumpK);
+    runTests<uint>(distributionType,fileName,startPower,stopPower,testCount,kDistribution,startK,stopK,jumpK, srandSeed, kDistributionSeed, randomSampleSeed, generatorSeed, arraySeed);
     break;
   default:
     printf("You entered and invalid option, now exiting\n");
